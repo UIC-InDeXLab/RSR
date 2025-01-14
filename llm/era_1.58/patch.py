09a6b95d638e2b3e33899a18d5a6ffeff3eccb8c
MohsenDehghankar <mohsendehghankar@gmail.com>
2025-01-14 18:56:30 +0000

    Add buffers for storing preprocessing vectors

--- llm/era_1.58/patch.py
+++ llm/era_1.58/patch.py
@@ -1,4 +1,35 @@
+import torch
 from transformers.quantizers import quantizer_bitnet
+from transformers.integrations import bitnet
+
+def init(self, in_features: int, out_features: int, bias: bool, device=None, dtype=None):
+        super().__init__()
+        self.dtype = dtype
+        self.in_features = in_features
+        self.out_features = out_features
+        self.register_buffer(
+            "weight",
+            torch.zeros(
+                (out_features // bitnet.VALUES_PER_ITEM, in_features),
+                dtype=torch.uint8,
+                device=device,
+            ),
+        )
+        self.register_buffer(
+            "weight_scale",
+            torch.ones(
+                (1),
+                dtype=dtype,
+                device=device,
+            ),
+        )
+        if bias:
+            self.register_buffer("bias", torch.zeros((out_features), dtype=dtype, device=device))
+        else:
+            self.bias = None
+        
+        self.register_buffer("permutations", torch.zeros(10))
+        self.register_buffer("segments", torch.zero(10))
 
 def preprocess_after_loading(self, model, *args, **kwargs):
     print(f"_process_model_after_weight_loading()")
@@ -26,4 +57,5 @@ def preprocess_after_loading(self, model, *args, **kwargs):
     return model
 
 def patch():
+    bitnet.BitLinear.__init__ = init
     quantizer_bitnet.BitNetHfQuantizer._process_model_after_weight_loading = preprocess_after_loading
